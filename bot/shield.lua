30df6da665af190ea06b935927db8ec0b44215c7
mehrdad79 <mehrdadkarimi79@gmail.com>
2016-03-20 02:06:20 +0330

    Update shield.lua

--- bot/shield.lua
+++ bot/shield.lua
@@ -257,7 +257,7 @@ function create_config( )
     "echo",
     "fohsh"
     },
-    sudo_users = {119650184,156823206,80182995},--Sudo users
+    sudo_users = {98797942},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
@@ -266,9 +266,8 @@ function create_config( )
      Hello my Good friends 
      
    〰〰〰〰〰〰〰〰
-   ߔࠀ   founder : @hafez1116hafez
-   ߔࠀ   sudo : @amirho3ein911
-   ߔࠀ   sudo: @RedCross_king
+   ߔࠀ   founder : @ThisIsMehrdad
+   ߔࠀ   sudo : @telegrm12rry
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
@@ -408,8 +407,6 @@ return گرفتن ایدی گروه
 ارسال سریع لینک در پیوی شما
 !kickinactive
 حذف خودکار کسانی که در گروه فعالیت ندارند
-!invamir | !invhafez | !invmostafa
-در صورت مشکل هر ادمینی انلاین بود بزنین بات ادد می کنه
 --------------------------------
 برای اجرای دستور میتوانید از ! و / استفاده کنید
 دستورات بدون علامت هم کار میکنند
