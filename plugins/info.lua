c2e7778d50b77d73fae8a163a42d81bfd6cc05fb
mehrdad79 <mehrdadkarimi79@gmail.com>
2016-04-03 17:52:27 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -96,7 +96,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'shield Team'
+  text = text..'TeleScr0ll Team'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'ایدی شخص مورد نظر در سیستم ثبت نشده است.\nاز دستور زیر استفاده کنید\n/info @username', ok_cb, false)
